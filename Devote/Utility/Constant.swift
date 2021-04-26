daade6e18a47cd372e0202d5e9cc3f781cf587d4
Samuel Brehm <samuelbrehm@gmail.com>
2021-04-26 16:47:42 -0300

    Create initial structure with icons, logos and files helpers

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Samuel Brehm on 26/04/21.
+//
+
+import SwiftUI
+
+// MARK: - Formatter
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
